fea8e718f8006f7a18952c7c4a88c8b46addd428
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-01 18:54:55 -0700

    when intent is killed by OS and tried to restart, it returns a null intent action, handle it by just creating the service

--- app/src/main/java/com/example/jared/simplebroadcasterreceiver/NotificationBroadcastReceiverService.java
+++ app/src/main/java/com/example/jared/simplebroadcasterreceiver/NotificationBroadcastReceiverService.java
@@ -26,6 +26,7 @@ public class NotificationBroadcastReceiverService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent.getAction().equals(null)) return START_STICKY;
         if (intent.getAction().equals(CustomActions.START_FOREGROUND_SERVICE)){
             initialized = true;
             Intent activityIntent = new Intent(this,MainActivity.class);
